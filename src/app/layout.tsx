18dc3f4821a6d60b7566f2859c2009a140588fea
3S Cavalcante <aderc19@gmail.com>
2025-03-02 18:02:09 -0300

    :lipstick: style: modificação na fonte utilizada.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,22 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Providers from "../components/Providers";
 import DynamicTitle from "@/components/DyncamicTitle";
+import { Inter } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Encurtador de URLs - FAB",
-  description: "Encurtador de URLs para o ambiente intraer da FAB.",
+  description: "Encurtador de URLs para o ambiente intranet da FAB.",
 };
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-inter",
+});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" className={inter.className}>
       <body>
         <Providers>
           <DynamicTitle />
